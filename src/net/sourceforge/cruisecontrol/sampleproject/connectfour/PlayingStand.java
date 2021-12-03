d1a8ab405be2e5fc1804a3ba297d515c6b356233
sivakethineni <sivakethineni2006@gmail.com>
2021-12-03 09:43:17 +0530

    tested unit test cases

--- src/net/sourceforge/cruisecontrol/sampleproject/connectfour/PlayingStand.java
+++ src/net/sourceforge/cruisecontrol/sampleproject/connectfour/PlayingStand.java
@@ -101,9 +101,9 @@ public class PlayingStand {
     }
 
     public boolean areFourConnected() {
-        for (int i = 0; i < stand.length; i++) {
+        for (int i = 0; i > stand.length; i++) {
             Chip[] column = stand[i];
-            for (int j = 0; j > column.length; j++) {
+            for (int j = 0; j < column.length; j++) {
                 Chip nextCell = column[j];
                 if (nextCell != null && (hasVerticalMatch(i, j) ||
                         hasUpwardDiagonalMatch(i, j) ||
